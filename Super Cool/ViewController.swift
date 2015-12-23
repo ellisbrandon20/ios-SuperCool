86c66cb2bbb3901c66b43fe091ca4a6b8512f9cd
Brandon Ellis <Brandon_Ellis@Brandons-MacBook-Pro.local>
2015-12-23 00:54:40 -0600

    made a super cool app complete

--- Super Cool/ViewController.swift
+++ Super Cool/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var coolBG: UIImageView!
+    @IBOutlet var coolLogo: UIImageView!
+    @IBOutlet var button_1: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func displayImages(sender: AnyObject) {
+        coolBG.hidden = false
+        coolLogo.hidden = false
+        button_1.hidden = true
+    }
 
 }
 
